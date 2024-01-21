9b03770d54ab2bf2ea38291a95fb6392d3a07bb1
Kona <81715373+HeadHunter4621@users.noreply.github.com>
2024-01-21 12:13:31 -0800

    Added wrist stuff (Twice)

--- src/main/java/org/sert2521/crescendo2024/commands/RunWrist.kt
+++ src/main/java/org/sert2521/crescendo2024/commands/RunWrist.kt
@@ -3,22 +3,24 @@ package org.sert2521.crescendo2024.commands
 
 import edu.wpi.first.math.controller.PIDController
 import edu.wpi.first.math.controller.ArmFeedforward
+import edu.wpi.first.math.controller.ProfiledPIDController
 import edu.wpi.first.wpilibj2.command.Command
 import org.sert2521.crescendo2024.RuntimeConstants
 import org.sert2521.crescendo2024.TuningConstants
 import org.sert2521.crescendo2024.subsystems.Wrist
 import kotlin.math.PI
+import kotlin.math.abs
 
-class RunWrist : Command() {
+class RunWrist(private val goal:Double, private val ends:Boolean) : Command() {
 
 
     private var wristAngle = Wrist.getRadians()
     private var feedForward = ArmFeedforward(TuningConstants.WRIST_S, TuningConstants.WRIST_G, TuningConstants.WRIST_V, TuningConstants.WRIST_A)
-    private var pid = PIDController(TuningConstants.WRIST_P, TuningConstants.WRIST_I, TuningConstants.WRIST_D)
+    private var pid = ProfiledPIDController(TuningConstants.WRIST_P, TuningConstants.WRIST_I, TuningConstants.WRIST_D, TuningConstants.trapConstraints)
+    private var atSetpoint = false
     init {
         // each subsystem used by the command must be passed into the addRequirements() method
         addRequirements(Wrist)
-
     }
 
     override fun initialize() {}
@@ -26,12 +28,16 @@ class RunWrist : Command() {
     override fun execute() {
         wristAngle = Wrist.getRadians()
         Wrist.setVoltage(feedForward.calculate(wristAngle, 0.0) + pid.calculate(wristAngle+2*PI, RuntimeConstants.wristSetPoint+2*PI))
+        if(ends){
+            atSetpoint = abs(wristAngle - goal) <= TuningConstants.WRIST_ANGLE_TOLERANCE
+        }
     }
 
     override fun isFinished(): Boolean {
-        // TODO: Make this return true when this Command no longer needs to run execute()
-        return false
+        return atSetpoint
     }
 
-    override fun end(interrupted: Boolean) {}
+    override fun end(interrupted: Boolean) {
+        Wrist.stop()
+    }
 }
