b293c93b845475198a32a3f72e9b5c69eced06ea
Kona <81715373+HeadHunter4621@users.noreply.github.com>
2024-01-25 17:37:37 -0800

    Added intake command, and tried to get isFinished to return getBeamBreak but it would always be red <|°_°|>

--- src/main/java/org/sert2521/crescendo2024/commands/Intake.kt
+++ src/main/java/org/sert2521/crescendo2024/commands/Intake.kt
@@ -1,6 +1,7 @@
 package org.sert2521.crescendo2024.commands
 
 import edu.wpi.first.wpilibj2.command.Command
+import org.sert2521.crescendo2024.subsystems.Indexer
 import org.sert2521.crescendo2024.subsystems.Intake
 
 class Intake : Command() {
@@ -20,6 +21,8 @@ class Intake : Command() {
     override fun isFinished(): Boolean {
         // TODO: Make this return true when this Command no longer needs to run execute()
         return false
+        //This is always red and I can't figure out why
+        //return Indexer.getBeamBreak()
     }
 
     override fun end(interrupted: Boolean) {
