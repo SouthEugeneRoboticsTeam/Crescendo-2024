4b996c24659ac153e54dc28b32fd93fb5ac78af8
Benji <benji.urbancic@gmail.com>
2024-03-09 10:06:36 -0800

    Day 1 Commit 10, Changed auto double plus a tiny amount and angle a tiny amount

--- src/main/java/org/sert2521/crescendo2024/Constants.kt
+++ src/main/java/org/sert2521/crescendo2024/Constants.kt
@@ -48,7 +48,7 @@ object PhysicalConstants{
     const val WRIST_SETPOINT_FAR = 0.295
     const val WRIST_SETPOINT_PODIUM_MINUS = 0.07
     const val WRIST_SETPOINT_PODIUM_PLUS = 0.215
-    var WRIST_SETPOINT_PODIUM_DOUBLE_PLUS = 0.293
+    var WRIST_SETPOINT_PODIUM_DOUBLE_PLUS = 0.291
 
     val field: AprilTagFieldLayout = AprilTagFields.k2023ChargedUp.loadAprilTagLayoutField()
     const val FIELD_WIDTH = 8.21
