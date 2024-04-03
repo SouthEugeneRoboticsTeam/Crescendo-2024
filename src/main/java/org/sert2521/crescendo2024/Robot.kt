8dbf7e85359f414eacbcbdf90d2c36c6e6fe26e2
Benji <benji.urbancic@gmail.com>
2024-04-02 19:39:25 -0700

    bro is this like my third commit that is just vision

--- src/main/java/org/sert2521/crescendo2024/Robot.kt
+++ src/main/java/org/sert2521/crescendo2024/Robot.kt
@@ -56,7 +56,7 @@ object Robot : TimedRobot()
     {
         CommandScheduler.getInstance().run()
         Output.update()
-        println(PhysicalConstants.aprilTagField.tags[0].pose)
+        //println(RuntimeConstants.isVisionAuto)
     }
 
     override fun disabledExit() {
--- src/main/java/org/sert2521/crescendo2024/commands/StartVision.kt
+++ src/main/java/org/sert2521/crescendo2024/commands/StartVision.kt
@@ -14,6 +14,7 @@ class StartVision : Command() {
 
     override fun initialize() {
         RuntimeConstants.isVisionAuto=true
+        println("ran")
     }
 
     override fun execute() {}
