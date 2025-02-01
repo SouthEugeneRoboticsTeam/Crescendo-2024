3e18aa2fa706d20413ece01204ba55e5af89495c
Kona <conradulysses@outlook.com>
2025-01-31 17:24:43 -0800

    Wheeeeee vision

--- src/main/java/org/sert2521/crescendo2024/subsystems/Drivetrain.kt
+++ src/main/java/org/sert2521/crescendo2024/subsystems/Drivetrain.kt
@@ -7,6 +7,7 @@ import com.revrobotics.spark.SparkMax
 import com.revrobotics.spark.config.SparkBaseConfig
 import com.revrobotics.spark.config.SparkMaxConfig
 import com.studica.frc.AHRS
+import edu.wpi.first.math.VecBuilder
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
@@ -20,6 +21,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase
 import org.sert2521.crescendo2024.SwerveConstants
 import org.sert2521.crescendo2024.SwerveModuleData
 import org.sert2521.crescendo2024.commands.JoystickDrive
+import org.sert2521.crescendo2024.libraries.LimelightHelpers
 import kotlin.math.*
 
 
@@ -219,6 +221,7 @@ object Drivetrain : SubsystemBase() {
         val deltaTime = currTime - prevTime
 
         poseEstimator.update(getYawAsRotation2d(), positionsArray)
+        visionEstimate()
 
         deltaPose = Pose2d((pose.y - prevPose.y) / deltaTime, (pose.x - prevPose.x) / deltaTime, -(pose.rotation - prevPose.rotation) / deltaTime)
 
@@ -317,6 +320,18 @@ object Drivetrain : SubsystemBase() {
         for (module in modules) {
             module.setCurrentLimit(amps, driveConfig)
         }
+    }
+
+    fun visionEstimate() {
+
+        var robotYaw = getYaw()
+        LimelightHelpers.SetRobotOrientation("", robotYaw, 0.0, 0.0, 0.0, 0.0, 0.0)
+
+        val limelightMeasurement: LimelightHelpers.PoseEstimate = LimelightHelpers.getBotPoseEstimate_wpiBlue("")
+
+        poseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(0.5, 0.5, 9999999999999.0))
+        poseEstimator.addVisionMeasurement(limelightMeasurement.pose, limelightMeasurement.timestampSeconds)
+
     }
 
     fun stop() {
