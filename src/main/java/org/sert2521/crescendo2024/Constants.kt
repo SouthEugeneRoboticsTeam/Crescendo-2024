0a61df969b9f5aa89fb5f17ef03ed7909fb073e4
Drive 1/Theodore <benji.urbancic@gmail.com>
2026-04-16 17:24:00 -0700

    Switched code for elementary demo

--- src/main/java/org/sert2521/crescendo2024/Constants.kt
+++ src/main/java/org/sert2521/crescendo2024/Constants.kt
@@ -78,19 +78,19 @@ object ConfigConstants{
     const val ROT_DEADBAND = 0.07
 
     //Drive speed constants
-    const val DRIVE_SPEED = 5.3
-    const val DRIVE_SECONDARY_SPEED = 2.75
-    const val ROT_SPEED = 6.0
-    const val ROT_SECONDARY_SPEED = 2.75
+    const val DRIVE_SPEED = 2.0
+    const val DRIVE_SECONDARY_SPEED = 1.0
+    const val ROT_SPEED = 2.0
+    const val ROT_SECONDARY_SPEED = 1.0
 
     //Acceleration of drivetrain
-    const val DRIVE_ACCEL = 35.0
+    const val DRIVE_ACCEL = 20.0
     const val DRIVE_DECCEL = 40.0
 
     const val DRIVE_OPTIMIZED = true
 
-    const val FLYWHEEL_IDLE_SPEED = 3000.0
-    const val FLYWHEEL_SHOOT_SPEED = 5000.0
+    const val FLYWHEEL_IDLE_SPEED = 1500.0
+    const val FLYWHEEL_SHOOT_SPEED = 1500.0
 }
 
 object SwerveConstants{
